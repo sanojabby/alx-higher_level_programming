b16252e07e0bb3eeaf5797e42e4006b90d044240
sanojabby <sanobaptiste2018@gmail.com>
2022-08-22 02:33:09 -0700

    scripts

---
+++ 0x00-python-hello_world/3-print_number.py
@@ -0,0 +1,7 @@
+str = "Python is an interpreted, interactive, object-oriented programming\
+         language that combines remarkable power with very clear syntax"
+         str = str[39:66] + ' ' + str[107:111] + ' ' + str[:6]
+         print(str)
+         #!/usr/bin/python3
+         number = 98
+         print(f"{number:d} Battery street")
