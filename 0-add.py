99ca27af9a15cceecf3bafe921cc8f27c0f6d60e
sanojabby <sanobaptiste2018@gmail.com>
2022-08-25 15:47:44 -0700

    1-calculation added

--- 0-add.py
+++ 0-add.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+from add_0 import add
+
 if __name__ == "__main__":
-     from add_0 import add
-     a = 1
-      b = 2
-      print("{} + {} = {}".format(a, b, add(a, b)))
+     """
+
+      Prints the result of the addition between two numbers
+
+       """
+       a = 1
+        b = 2
+         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
--- 1-calculation.py
+++ 1-calculation.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-     from calculator_1 import add, sub, mul, div
-      a = 10
-      b = 5
-      print("{} + {} = {}".format(a, b, add(a, b)))
-       print("{} - {} = {}".format(a, b, sub(a, b)))
-       print("{} * {} = {}".format(a, b, mul(a, b)))
-        print("{} / {} = {}".format(a, b, div(a, b)))
+from calculator_1 import add, sub, mul, div
+
+if __name__ == '__main__':
+     """
+
+      Prints the result of the addition, substract, multiplication
+       and division between two numbers
+
+        """
+        a = 10
+        b = 5
+         print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+         print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+         print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+         print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
