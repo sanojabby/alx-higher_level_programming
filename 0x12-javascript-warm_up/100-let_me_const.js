5f1385cb65d1e92744a25b39e7ee1d122a5153d2
sanojabby <sanobaptiste2018@gmail.com>
2022-11-28 20:00:12 -0800

    add

---
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+
+'100-let_me_const'.exports;
+
+myVar = 333;
---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+
+exports.addMeMaybe = function (number, theFunction) {
+
+  theFunction(number + 1);
+
+};
