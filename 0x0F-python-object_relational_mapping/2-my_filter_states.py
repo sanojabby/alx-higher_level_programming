3b57432feb99eae62c787e7d612cb7ef19199a6a
sanojabby <sanobaptiste2018@gmail.com>
2022-12-04 01:08:11 -0800

    added

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,52 +2,37 @@
 
 """
 
-a script that takes in an argument and displays all values in
+Lists all values in the states tables of a database where name
 
-the states table of hbtn_0e_0_usa where name matches the argument
+matches the argument
 
 """
 
+import sys
 
 import MySQLdb
 
-from sys import argv
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+
+                         db=sys.argv[3], port=3306)
 
 
-if __name__ == "__main__":
+    cur = db.cursor()
+
+    cur.execute("SELECT * \
+
+    FROM states \
+
+    WHERE CONVERT(`name` USING Latin1) \
+
+    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
+
+    states = cur.fetchall()
 
 
-    # connect to database
+    for state in states:
 
-    db = MySQLdb.connect(host="localhost",
-
-                         port=3306,
-
-                         user=argv[1],
-
-                         passwd=argv[2],
-
-                         db=argv[3])
-
-
-    # create cursor to exec queries using SQL; match arg given
-
-    cursor = db.cursor()
-
-    sql_cmd = "SELECT * \
-
-                 FROM states \
-
-                 WHERE name LIKE '{:s}' ORDER BY id ASC".format(argv[4])
-
-    cursor.execute(sql_cmd)
-
-    for state in cursor.fetchall():
-
-        if state[1] == argv[4]:
-
-            print(state)
-
-    cursor.close()
-
-    db.close()
+        print(state)
